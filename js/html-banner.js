ef0858ee872bfbf297246e48912c02b7bd5e9066
Arnar Yngvason <arnar@hvitahusid.is>
2013-10-17 17:15:52 +0000

    init

---
+++ js/html-banner.js
@@ -0,0 +1,35 @@
+(function(window, document, $, undefined) {
+    'use scrict';
+
+    var referer, ref_host;
+
+    var url = purl(true);
+
+    if(url.param('referer')) {
+        referer = url.param('referer');
+    } else {
+        try {
+            referer = top.location.href;
+        } catch(e) {
+            referer = document.referrer;
+        }
+    }
+    referer = purl(referer, true);
+
+    referer_str = [referer.attr('host'), referer.attr('path')].join('');
+
+    var href = purl(url.param('href'), true);
+    var href_params = href.param();
+    href_params.referer = referer_str;
+
+    var href_str = href.attr('protocol') + '://' + href.attr('host') + href.attr('path') + '?' + $.param(href_params);
+
+    $(document).on('click', function() {
+        if(typeof ga !== 'undefined') {
+            ga('send', 'event', referer_str, 'click');
+        }
+
+        window.open(href_str, '_NEW').focus();
+    });
+
+})(window, document, jQuery);
